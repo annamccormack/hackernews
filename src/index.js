9931a15253f3ec15c0eaafb8efe528a3c5f2a0e0
Anna McCormack <anna.mccormack94@gmail.com>
2021-03-30 19:43:37 +1300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+const path = require('path')
+const { PrismaClient } = require('@prisma/client')
+const { ApolloServer, PubSub } = require('apollo-server')
+const { getUserId } = require('./utils')
+
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
+
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Link,
+  Subscription
+}
+
+const prisma = new PrismaClient()
+const pubsub = new PubSub()
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
+  resolvers,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      pubsub,
+      userId:
+        req && req.headers.authorization
+          ? getUserId(req)
+          : null
+
+    }
+  }
+})
+
+server
+  .listen()
+  .then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+  )
---
+++ src/resolvers/Query.js
@@ -0,0 +1,16 @@
+async function feed(parent, args, context, info) {
+  const where = args.filter
+    ? {
+      OR: [
+        { description: { contains: args.filter } },
+        { url: { contains: args.filter } },
+      ],
+    }
+    : {}
+
+  const links = await context.prisma.link.findMany({
+    where,
+  })
+
+  return links
+}
